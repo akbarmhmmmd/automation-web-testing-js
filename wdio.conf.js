f5825dcd623d756a2b596d2998dfdf66b1f59aeb
Muhammad Akbar <akbarmhmmmd@gmail.com>
2023-07-26 20:31:02 +0700

    feat: init

--- wdio.conf.js
+++ wdio.conf.js
@@ -129,7 +129,7 @@ exports.config = {
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
         // <string[]> (file/dir) require files before executing features
-        require: ['./webtesting/step-definitions/login.step.js'],
+        require: ['./webtesting/step-definitions/*.js'],
         // <boolean> show full backtrace for errors
         backtrace: false,
         // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
