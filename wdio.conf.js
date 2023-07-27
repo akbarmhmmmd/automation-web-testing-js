0d42b14464afa682efe7649c318767e6945d1cba
Muhammad Akbar <akbarmhmmmd@gmail.com>
2023-07-27 09:26:12 +0700

    fix: fixing test file structure

--- wdio.conf.js
+++ wdio.conf.js
@@ -22,7 +22,7 @@ exports.config = {
     // will be called from there.
     //
     specs: [
-        './webtesting/**/*.feature'
+        './test/**/**/*.feature'
     ],
     // Patterns to exclude.
     exclude: [
@@ -129,7 +129,7 @@ exports.config = {
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
         // <string[]> (file/dir) require files before executing features
-        require: ['./webtesting/step-definitions/*.js'],
+        require: ['./test/**/step-definitions/*.js'],
         // <boolean> show full backtrace for errors
         backtrace: false,
         // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
